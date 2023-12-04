446c52d40cd5a84d5462ae25509d0e6571703fb6
Josh Creek <8179928+jcreek@users.noreply.github.com>
2023-12-04 20:12:28 +0000

    feat(*): Add logo

--- azure-function/Logo.cs
+++ azure-function/Logo.cs
@@ -5,13 +5,13 @@ using Microsoft.Azure.Functions.Worker.Http;
 public class Logo
 {
     [Function("GetLogo")]
-    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "logo.png")] HttpRequestData req)
+    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "logo.jpeg")] HttpRequestData req)
     {
-        // Return logo.png that's in the root of the project
+        // Return logo.jpeg that's in the root of the project
         var response = req.CreateResponse(HttpStatusCode.OK);
-        response.Headers.Add("Content-Type", "image/png");
+        response.Headers.Add("Content-Type", "image/jpeg");
 
-        var logo = System.IO.File.ReadAllBytes("logo.png");
+        var logo = System.IO.File.ReadAllBytes("logo.jpeg");
         response.Body.Write(logo);
 
         return response;
